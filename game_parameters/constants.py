0af50d282446c8145f1d2eca0076d1109dbeffa8
Daniel <daniel.sal.oneil@gmail.com>
2020-01-01 10:27:25 -0800

    Fixed lack of horse coat diversity.
    The issue was that for most genes, only the dominant allele was being seen. There are now more recessive alleles.

--- game_parameters/constants.py
+++ game_parameters/constants.py
@@ -4,11 +4,15 @@ import json
 # Genetics
 GENE_LENGTH = 6  # Basepairs per gene
 CHROMOSOME_LENGTH = 20  # Genes per chromosome
-DEFAULT_WELL_FORMED_CUTOFF = 0  # Default value for difficulty of a gene being well formed.
+DEFAULT_WELL_FORMED_CUTOFF = .1  # Default value for difficulty of a gene being well formed.
                                 # Higher is harder.
 folder = os.path.dirname(__file__)
-with open(os.path.join(folder, 'genes.json'), 'r') as f:
-    GENES = json.load(f)
+try:
+    with open(os.path.join(folder, 'local_genes.json'), 'r') as f:
+        GENES = json.load(f)
+except FileNotFoundError:
+    with open(os.path.join(folder, 'genes.json'), 'r') as f:
+        GENES = json.load(f)
 
 # Naming
 HORSE_NAME_MAX = 24  # Longest permitted name for a horse
